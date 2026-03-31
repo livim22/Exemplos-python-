fcf2f10274d687d48b17989e24d42731b2c9141b
Lívia Melissa <livimel042210@gmail.com>
2026-03-31 11:11:46 -0300

    Add files via upload

---
+++ 25-pokemon.py
@@ -0,0 +1,39 @@
+import os
+os.system("cls)")
+vida_jogador = 100
+vida_inimigo = 100
+
+print("Escolha o seu pokemon")
+print("1 - Pikachu")
+print("2 - Bubasauro")
+print("1 - Charmander")
+
+while vida_jogador >= 0 and vida_inimigo >= 0: 
+
+
+    jogando = True
+
+while jogando == True:
+    print("Bem-vindo ao jogo de batalha!")
+    print("1. Atacar")
+    print("2. curar")
+    print("3. Fugir")
+    escolha = input("Escolha sua ação: ")
+
+    if escolha == "1":
+        print("Você atacou o inimigo!")
+        vida_inimigo -=10
+    elif escolha == "2":
+        print("Você se curou!")
+        vida_jogador += 5
+    elif escolha == "3":
+        print("Você fugiu do combate!")
+        jogando = False
+    else:
+        print("Tente novamente!")
+    if vida_inimigo <= 0:
+        print("Parabéns! Você venceu a batalha!")
+        jogando = False
+    elif vida_jogador <= 0:
+        print("Você foi derrotado! Tente novamente.")
+        jogando = False
